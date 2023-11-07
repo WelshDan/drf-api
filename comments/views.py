08e8d2bd641db5f7ebb6c710fcf0bdf9b1a7e6a3
WelshDan <>
2023-11-07 13:08:46 +0000

    Add comments app, create serializers and views

---
+++ comments/views.py
@@ -0,0 +1,14 @@
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer, CommentDetailSerializer
+
+
+class CommentList(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+
+    def perform_create(self, Serializer):
+        serializer.save(owner=self.request.user)
+
--- posts/serializers.py
+++ posts/serializers.py
@@ -10,15 +10,15 @@ class PostSerializer(serializers.ModelSerializer):
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
-            raise serializer.ValidationError(
+            raise serializers.ValidationError(
                 'Image size larger than 2MB'
             )
         if value.image.width > 4096:
-            raise serializer.ValidationError(
+            raise serializers.ValidationError(
                 'Image width larger than 4096px'
             )
         if value.image.height > 4096:
-            raise serializer.ValidationError(
+            raise serializers.ValidationError(
                 'Image height larger than 4096px'
             )
         return value
@@ -34,4 +34,3 @@ class PostSerializer(serializers.ModelSerializer):
             'created_at', 'updated_at', 'title', 'content', 'image',
             'image_filter'
         ]
-    
